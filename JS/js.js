53612d99f46626900e19e2bb34c7613efea8cb5e
Mollis <ram.takemura@gmail.com>
2023-05-07 10:30:41 -0700

    Update

--- JS/js.js
+++ JS/js.js
@@ -11,104 +11,137 @@
 
 
 // let result = '';
-// const length = 7;
+// // const length = 7;
 
-// for (let i = 1; i < length; i++) {
-//     for (let j = 0; j < i; j++) {
-//         result += '*';
+// // for (let i = 1; i < length; i++) {
+// //     for (let j = 0; j < i; j++) {
+// //         result += '*';
+// // //     }
+
+// // //     result += '\n';
+// // // }
+
+// // // console.log(result);
+
+
+// // // first: for (let i = 0; i < 3; i++) {
+// // //         console.log(`First level ${i}`);
+// // //         for (let j = 0; j < 3; j++) {
+// // //             console.log(`Second level ${j}`);
+// // //             for (let k = 0; k < 3; k++) {
+// // //                 if (k === 2) continue first;
+// // //                 console.log(`Third level ${k}`);
+// // //             }
+// // //         }
+        
+// // //         console.log(i);
+// // //     };
+
+
+
+
+// // function firstTask() {
+// //   // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+// //   const arr = [3, 5, 8, 16, 20, 23, 50];
+// //   const result = [];
+
+// //   // Пишем решение вот тут
+// //   for (let i = 0; i < arr.length; i++) {
+// //     console.log(result[i] = arr[i] )
+// //   }
+  
+// //   // Не трогаем
+// //   return result;
+// // }
+
+// // // Место для второй задачи
+// // function secondTask() {
+// //   // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+// //   const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+// //   // Пишем решение вот тут
+// //   for (let i = 0; i < data.length; i++) {
+// //     if (typeof data[i] === 'number') {
+// //        console.log (data[i] * 2);
+// //       } else if (typeof data[i] === 'string')  {
+// //       console.log( data[i] + ' - done' )
+// //      }
 // //     }
+// //   }
+   
+// //   // Не трогаем
+// //   return data;
+// // }
 
-// //     result += '\n';
+// // // Место для третьей задачи
+// // function thirdTask() {
+// //   // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+// //   const data = [5, 10, 'Shopping', 20, 'Homework'];
+// //   const result = [];
+
+// //  for (let i = 4; i >=0; i--) {
+// //     result.push(data[i]);
+// //  }
+
+// //   return result;
+// // }
+
+
+
+// //      *
+// //     ***
+// //    *****
+// //   *******
+// //  *********
+// // ***********
+
+
+// // const lines = 5;
+// // let result = '';
+
+// // for (let i = 0; i <= 6; i++) {
+
+// //   for (let j = lines; j >= i; j-- ) {
+// //     result += ' ';
+// //   }
+  
+// //   for (let k = 0; k <= i; k++) {
+// //     result += '*';
+// //   }
+// //   result += '\n';
 // // }
 
 // // console.log(result);
+// let num = 20;
 
-
-// // first: for (let i = 0; i < 3; i++) {
-// //         console.log(`First level ${i}`);
-// //         for (let j = 0; j < 3; j++) {
-// //             console.log(`Second level ${j}`);
-// //             for (let k = 0; k < 3; k++) {
-// //                 if (k === 2) continue first;
-// //                 console.log(`Third level ${k}`);
-// //             }
-// //         }
-        
-// //         console.log(i);
-// //     };
-
-
-
-
-// function firstTask() {
-//   // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-//   const arr = [3, 5, 8, 16, 20, 23, 50];
-//   const result = [];
-
-//   // Пишем решение вот тут
-//   for (let i = 0; i < arr.length; i++) {
-//     console.log(result[i] = arr[i] )
-//   }
-  
-//   // Не трогаем
-//   return result;
+// function showFirstMessage(text) {
+//     console.log(text);
+//      let num = 10;
+//      console.log(num);
 // }
 
-// // Место для второй задачи
-// function secondTask() {
-//   // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-//   const data = [5, 10, 'Shopping', 20, 'Homework'];
+// showFirstMessage('Hello World');
+// console.log(num);
 
-//   // Пишем решение вот тут
-//   for (let i = 0; i < data.length; i++) {
-//     if (typeof data[i] === 'number') {
-//        console.log (data[i] * 2);
-//       } else if (typeof data[i] === 'string')  {
-//       console.log( data[i] + ' - done' )
-//      }
-//     }
-//   }
-   
-//   // Не трогаем
-//   return data;
-// }
 
-// // Место для третьей задачи
-// function thirdTask() {
-//   // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-//   const data = [5, 10, 'Shopping', 20, 'Homework'];
-//   const result = [];
+//  function ret() {
+//     let num = 50;
 
-//  for (let i = 4; i >=0; i--) {
-//     result.push(data[i]);
+// /////////////////////////////////
+//     return num;
 //  }
 
-//   return result;
-// }
+//  const anotherNum = ret();
+//  console.log(anotherNum);
 
 
 
-//      *
-//     ***
-//    *****
-//   *******
-//  *********
-// ***********
+//  const logger = function() {
+//     console.log('Hello');
+//  };
 
 
-// const lines = 5;
-// let result = '';
+//  logger();
 
-// for (let i = 0; i <= 6; i++) {
 
-//   for (let j = lines; j >= i; j-- ) {
-//     result += ' ';
-//   }
-  
-//   for (let k = 0; k <= i; k++) {
-//     result += '*';
-//   }
-//   result += '\n';
-// }
-
-// console.log(result);
+//  const calc = (a, b) => {return a + b};
