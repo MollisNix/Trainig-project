9677b0ba81552ebaf1e90b906d17b53da0082a53
Mollis <ram.takemura@gmail.com>
2023-05-07 12:37:13 -0700

    complete tusk

--- JS/Practice.js
+++ JS/Practice.js
@@ -17,51 +17,51 @@
 // console.log(personalMovieDB);
 
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false
-};
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false
+// };
 
-if (personalMovieDB.count < 10) {
-  alert('You have watched so little ');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30 ) {
-  alert('You are the common viwer');
-} else {
-  alert('You are cinephile');
-}
+// if (personalMovieDB.count < 10) {
+//   alert('You have watched so little ');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30 ) {
+//   alert('You are the common viwer');
+// } else {
+//   alert('You are cinephile');
+// }
 
 
 
+// // const a = prompt('Один из последних просмотренных фильмов?', ''),
+// //       b = +prompt('На сколько оцениваете его?', ''),
+// //       c = prompt('Один из последних просмотренных фильмов?', ''),
+// //       d = +prompt('На сколько оцениваете его?', '');  & b === '' || b === null || b.length > 50
+
+// for (let i = 0; i < 2; i++) {
 // const a = prompt('Один из последних просмотренных фильмов?', ''),
-//       b = +prompt('На сколько оцениваете его?', ''),
-//       c = prompt('Один из последних просмотренных фильмов?', ''),
-//       d = +prompt('На сколько оцениваете его?', '');  & b === '' || b === null || b.length > 50
+//     b = +prompt('На сколько оцениваете его?', '');
 
-for (let i = 0; i < 2; i++) {
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = +prompt('На сколько оцениваете его?', '');
-
-      if (a != null &&  b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-      } else {
-        i--;
-      }
+//       if (a != null &&  b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//       } else {
+//         i--;
+//       }
       
-    //  if (a === '' || a === null || a.length > 50 || b === '' || b === null || b.length > 50) {
-    //     i--;
-    //     a = prompt('Один из последних просмотренных фильмов?', '');
-    //     b = +prompt('На сколько оцениваете его?', '');
-    //    }
-    //  personalMovieDB.movies[a] = b;
-}
+//     //  if (a === '' || a === null || a.length > 50 || b === '' || b === null || b.length > 50) {
+//     //     i--;
+//     //     a = prompt('Один из последних просмотренных фильмов?', '');
+//     //     b = +prompt('На сколько оцениваете его?', '');
+//     //    }
+//     //  personalMovieDB.movies[a] = b;
+// }
 
 
 
 
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
