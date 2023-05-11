edde722869c89fddc6bc936f096c4ca06f449cde
Mollis <ram.takemura@gmail.com>
2023-05-11 16:31:34 -0700

    Done another practice tusk

--- JS/Practice.js
+++ JS/Practice.js
@@ -12,56 +12,100 @@
 
 // let a = prompt('Один из последних просмотренных фильмов?', ''),
 //     b = +prompt('На сколько оцениваете его?', '');
-//movies[a] = b;
+// movies[a] = b;
 
 // console.log(personalMovieDB);
 
 
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
 
-// const personalMovieDB = {
-//   count: numberOfFilms,
-//   movies: {},
-//   actors: {},
-//   genres: [],
-//   privat: false
-// };
+function start() {
+     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// if (personalMovieDB.count < 10) {
-//   alert('You have watched so little ');
-// } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30 ) {
-//   alert('You are the common viwer');
-// } else {
-//   alert('You are cinephile');
-// }
+     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+     }
+}
 
 
 
-// // const a = prompt('Один из последних просмотренных фильмов?', ''),
-// //       b = +prompt('На сколько оцениваете его?', ''),
-// //       c = prompt('Один из последних просмотренных фильмов?', ''),
-// //       d = +prompt('На сколько оцениваете его?', '');  & b === '' || b === null || b.length > 50
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
 
-// for (let i = 0; i < 2; i++) {
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert('You have watched so little ');
+      } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30 ) {
+        alert('You are the common viwer');
+      } else {
+        alert('You are cinephile');
+      }
+}
+
+
+
+
 // const a = prompt('Один из последних просмотренных фильмов?', ''),
-//     b = +prompt('На сколько оцениваете его?', '');
-
-//       if (a != null &&  b != null && a != '' && b != '' && a.length < 50) {
-//         personalMovieDB.movies[a] = b;
-//       } else {
-//         i--;
-//       }
-      
-//     //  if (a === '' || a === null || a.length > 50 || b === '' || b === null || b.length > 50) {
-//     //     i--;
-//     //     a = prompt('Один из последних просмотренных фильмов?', '');
-//     //     b = +prompt('На сколько оцениваете его?', '');
-//     //    }
-//     //  personalMovieDB.movies[a] = b;
-// }
+//       b = +prompt('На сколько оцениваете его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = +prompt('На сколько оцениваете его?', '');  & b === '' || b === null || b.length > 50
 
 
 
 
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = +prompt('На сколько оцениваете его?', '');
+        
+              if (a != null &&  b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+              } else {
+                i--;
+              }
+              
+            //  if (a === '' || a === null || a.length > 50 || b === '' || b === null || b.length > 50) {
+            //     i--;
+            //     a = prompt('Один из последних просмотренных фильмов?', '');
+            //     b = +prompt('На сколько оцениваете его?', '');
+            //    }
+            //  personalMovieDB.movies[a] = b;
+        }
+}
 
-// console.log(personalMovieDB);
+
+
+
+function showMyDB() {
+    if (personalMovieDB.privat === false) {
+        console.log(personalMovieDB);
+    }
+}
+
+
+function writeYourGenres(data) {
+    let yourGenres;
+    for (let i = 0; i < 3; i++) {
+        yourGenres = prompt(`Ваш любимый жанр под номром ${i + 1}`, ``);
+        
+        data.genres[i] = yourGenres;
+        console.log(data.genres);
+    }
+}
+
+
+
+
+
+start();
+detectPersonalLevel();
+rememberMyFilms();
+writeYourGenres(personalMovieDB);
+showMyDB();
--- JS/js.js
+++ JS/js.js
@@ -292,3 +292,29 @@
 // console.log(getMathResult(5, 10) );
 // console.log(getMathResult(6, 10) );
 
+
+// const str = 'test';
+
+// // console.log(str.toLowerCase() );
+// console.log(str);
+
+// const fruit = 'Some fruit';
+
+// console.log(fruit.indexOf('q'));
+
+// const logg = 'Hello world';
+
+// // console.log(logg.slice(6, 10) );
+
+// console.log(logg.substring(6, 11) );
+// console.log(logg.substr(6, 5) );
+
+
+
+// const num = 12.2;
+// console.log(Math.round(num) );
+
+// const test = '12.2px';
+// console.log(parseInt(test) );
+// console.log(parseFloat(test) );
+
