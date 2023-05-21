e7a6b761ad70d6e976f1c502726cd37f8181ae37
Mollis <ram.takemura@gmail.com>
2023-05-20 20:58:09 -0700

    new commit

--- JS/js.js
+++ JS/js.js
@@ -525,97 +525,193 @@
 
 // const options = {
 //     name: 'test',
-//     width: 1024,
-//     height: 1024,
-//     colors: {
-//         border: 'black',
-//         background: 'red'
-//     },
-//     makeTest: function() {
-//         console.log('Test')
-//     }
-// };
-
-// options.makeTest();
-
-// const {border, background} = options.colors;
-
-// console.log(border);
-
-
-
-// // console.log(Object.keys(options).length );
-
-// // console.log(options.name);
-
-// // delete options.name;
-// // console.log(options);
-
-
-
-// // let counter = 0;
-
-// // for (let key in options) {
-// //     if (typeof(options[key]) === 'object') {
-// //         for (let i in options[key]) {
-// //             console.log(`Value  ${i} have  znchenie ${options[key][i]}`)
-// //         }
-// //     } else {
-// //         console.log(`Value  ${key} have  znchenie ${options[key]}`)
-// //         counter++
+// //     width: 1024,
+// //     height: 1024,
+// //     colors: {
+// //         border: 'black',
+// //         background: 'red'
+// //     },
+// //     makeTest: function() {
+// //         console.log('Test')
 // //     }
+// // };
+
+// // options.makeTest();
+
+// // const {border, background} = options.colors;
+
+// // console.log(border);
+
+
+
+// // // console.log(Object.keys(options).length );
+
+// // // console.log(options.name);
+
+// // // delete options.name;
+// // // console.log(options);
+
+
+
+// // // let counter = 0;
+
+// // // for (let key in options) {
+// // //     if (typeof(options[key]) === 'object') {
+// // //         for (let i in options[key]) {
+// // //             console.log(`Value  ${i} have  znchenie ${options[key][i]}`)
+// // //         }
+// // //     } else {
+// // //         console.log(`Value  ${key} have  znchenie ${options[key]}`)
+// // //         counter++
+// // //     }
     
+// // // }
+
+// // // console.log(counter);
+
+
+// // let someObj = {
+// //     kids: 2,
+// //     animals: 3,
+// //     food: {
+// //         apple: 3,
+// //         lemon: 2,
+// //     }
 // // }
 
-// // console.log(counter);
+
+// // for (key in someObj) {
+// //     console.log(someObj[key]);
+// // }
 
 
-// let someObj = {
-//     kids: 2,
-//     animals: 3,
-//     food: {
-//         apple: 3,
-//         lemon: 2,
+// const arr = [1, 15, 3, 6, 18];
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b) {
+//     return a - b;
+// }
+// // arr.forEach(function(item, index, arr) {
+// //     console.log(`${index}: ${item} внутри масива ${arr}`)
+// // });
+
+// // arr[99] = 0;
+// // console.log(arr.length)
+// // console.log(arr)
+
+
+// // arr.pop()
+// // arr.push('ffr')
+
+// // console.log(arr)
+
+// // for (let i = 0; i < arr.length; i++) {
+// //     console.log(arr[i]);
+// // }
+
+// // for (let value of arr) {
+// //     console.log(value)
+// // }
+
+
+// // const str = prompt('', '');
+// // const products = str.split(', ');
+// // products.sort();
+// // console.log(products.join('; '));
+
+// let a = 5,
+//     b = a;
+
+// b = b + 5;
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//     a: 5,
+//     b: 1,
+// };
+
+// // const copy = obj;
+
+// // copy.a = 10
+
+// // console.log(copy);
+// // console.log(obj)
+
+
+// function copy(mainOBJ) {
+//     let objCopy =  {};
+
+
+//     for (let key in mainOBJ) {
+//         objCopy[key] = mainOBJ[key];
+//     } 
+
+//     return objCopy;
+// }
+
+// const number = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7, 
+//         y: 4
 //     }
 // }
 
+// // const newNumbers = copy(number);
+// // newNumbers.a = 11;
+// // newNumbers.c.x = 112;
+// // console.log(newNumbers);
+// // console.log(number);
 
-// for (key in someObj) {
-//     console.log(someObj[key]);
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+
+// const clone = Object.assign({}, add);
+
+// clone.d = 20;
+// // console.log(add);
+// // console.log(clone);
+
+
+// const oldArray = ['a', 'b', 'c'];
+
+// const newArray = oldArray.slice();
+// newArray[1] = 'asasasasas';
+// console.log(newArray);
+// console.log(oldArray);
+
+
+// const video = ['youtube', 'vimeo', 'rutube'],
+//       blogs = ['wordpress', 'Livejournal', 'blogger'],
+//       internet = [...video, ...blogs, 'vk', 'facebook'];
+
+//       console.log(internet);
+
+// function log (a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+
 // }
 
+// const num  = [2, 5, 7];
 
-const arr = [1, 15, 3, 6, 18];
-arr.sort(compareNum);
-console.log(arr);
+// log(...num);
 
-function compareNum(a, b) {
-    return a - b;
-}
-// arr.forEach(function(item, index, arr) {
-//     console.log(`${index}: ${item} внутри масива ${arr}`)
-// });
+// const array = ['a', 'b'];
+// const newArra = [...array];
 
-// arr[99] = 0;
-// console.log(arr.length)
-// console.log(arr)
+// const q = {
+//     one: 1,
+//     two: 2
+// };
 
+// const newObj = {...q};
 
-// arr.pop()
-// arr.push('ffr')
+// console.log(newObj);
 
-// console.log(arr)
-
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
-
-// for (let value of arr) {
-//     console.log(value)
-// }
-
-
-// const str = prompt('', '');
-// const products = str.split(', ');
-// products.sort();
-// console.log(products.join('; '));
